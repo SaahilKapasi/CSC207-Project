5f5ec7f6004d2e319c96b5edde43a966bcd30f1e
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2024-10-24 09:32:54 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/hifive/",
+  build: {
+    outDir: "./dist/hifive",
+  },
   server: {
     proxy: {
       "/hifive/api": {
