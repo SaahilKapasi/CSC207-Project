2ce648917e30cc4f39c81396cf35d4fc0317c2e6
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2024-10-24 00:45:32 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hifive/",
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000"
-      }
+        target: "https://cashapp-dashboard-f15aa2695eea.herokuapp.com/",
+      },
     },
   },
   plugins: [react()],
-})
+});
