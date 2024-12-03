302cdb8fcc162452ad29beb9af4385c22d981add
CodeYimin <56568926+CodeYimin@users.noreply.github.com>
2024-12-03 17:53:03 -0500

    Combine both "welcome to bias visualizer"

--- view/src/components/WelcomeSession.tsx
+++ view/src/components/WelcomeSession.tsx
@@ -10,7 +10,7 @@ export default function WelcomeSection({
   return (
     <>
       <div
-        className="h-screen bg-cover bg-center flex items-center justify-center"
+        className="h-screen bg-cover bg-center flex flex-col items-center justify-center text-center gap-2"
         style={{
           backgroundImage: `url(logo.svg)`, // Replace with your image path
         }}
@@ -18,27 +18,17 @@ export default function WelcomeSection({
         <h1 className="text-white text-4xl font-bold bg-black bg-opacity-50 px-4 py-2 rounded">
           Welcome to Bias Visualizer!
         </h1>
-      </div>
-      <div
-        id="welcome"
-        className="h-screen flex flex-col items-center justify-center bg-gray-50"
-      >
-        <div className="text-center max-w-xl">
-          <h1 className="text-4xl font-bold mb-4">
-            Welcome to Bias Visualizer!
-          </h1>
-          <p className="text-lg text-gray-700 mb-6">
-            Today we spot bias and together provide an ethical environment. Use
-            this to get a better sense of your ML model and protect people from
-            undesired harms!
-          </p>
-          <button
-            onClick={onWelcome}
-            className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600"
-          >
-            Get Started
-          </button>
-        </div>
+        <p className="text-lg text-white p-1 rounded max-w-xl bg-black bg-opacity-50">
+          Today we spot bias and together provide an ethical environment. Use
+          this to get a better sense of your ML model and protect people from
+          undesired harms!
+        </p>
+        <button
+          onClick={onWelcome}
+          className="px-6 py-3 bg-green-500 text-white rounded-lg hover:bg-green-600"
+        >
+          Get Started
+        </button>
       </div>
     </>
   );
